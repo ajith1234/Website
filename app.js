06133da0a599cdd3a2cc54e1dd3ddf9e27ec149d
ajith1234 <uk.ajith@gmail.com>
2017-10-18 18:31:43 +0100

    node server + Bootstrap

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+    console.log("listening on port 8080...");
 });
 
 app.listen(8080);
