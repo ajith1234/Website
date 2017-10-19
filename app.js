f5ca62aeda8383da9f78abcc8d2fbb7ed566e064
ajith1234 <uk.ajith@gmail.com>
2017-10-19 23:57:37 +0100

    data base and post method added

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express')
+const app = express()
+const bodyParser= require('body-parser')
+const path = require('path')
+const MongoClient = require('mongodb').MongoClient
+var db
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use('/public', express.static(__dirname + '/public'))
+app.set('view engine', 'ejs')
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-    console.log("listening on port 8080...");
-});
+    db.collection('quotes').find().toArray((err, result) => {
+        res.render(__dirname+'/public/views/index.ejs', {quotes: result})
+      })
+  
+})
 
-app.listen(8080);
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+      if (err) return console.log(err)
+  
+      console.log('saved to database')
+      res.redirect('/')
+    })
+})
+
+app.listen(8080, function(){
+    console.log("Listening on port 8080...")
+})
+
+
+
+MongoClient.connect('mongodb://127.0.0.1:27017', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 27017...')
+  })
+})
+
+
